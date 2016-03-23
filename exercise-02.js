e9036166c1b18695678209ee51260d6855481c42
Luis Ibanez <luis.ibanez@gmail.com>
2016-03-22 22:53:32 -0400

    Nest an address object inside the student.

---
+++ exercise-02.js
@@ -0,0 +1,21 @@
+var student = {
+ name: "John",
+ email: "john@gmail.com",
+ phone: "518-444-5555",
+ address: "33 Christopher St, New York, NY"
+};
+
+
+/* 
+
+Exercise 02: Restructure the "address" attribute to be an object with attributes
+
+  number
+  street
+  city
+  state
+  zip
+
+Then write the console.log lines to print out all its attributes.
+
+*/
