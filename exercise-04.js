5d2eb6e79f04e0f2916d0b3776612ea3440a4bab
Luis Ibanez <luis.ibanez@gmail.com>
2016-03-22 23:15:13 -0400

    Add exercise to include a list classes function.

---
+++ exercise-04.js
@@ -0,0 +1,28 @@
+var student = {
+ name: "John",
+ email: "john@gmail.com",
+ phone: "518-444-5555",
+ address: {
+   number: 33,
+   street: "Christopher St",
+   city: "New York",
+   state: "NY",
+   zip: "12345"
+  },
+ classes: [
+    "math",
+    "history",
+    "design",
+    "accounting" 
+  ]
+};
+
+
+/* 
+
+Exercise 04: 
+
+    * Add an attribute "printTopings" 
+    * Make it to be a function that list the student's classes.
+
+*/
