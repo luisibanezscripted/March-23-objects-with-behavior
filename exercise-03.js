e1aab6acad715a546864203dbc882f01bd65b634
Luis Ibanez <luis.ibanez@gmail.com>
2016-03-22 22:59:25 -0400

    Added a list as an attribute.

---
+++ exercise-03.js
@@ -0,0 +1,26 @@
+var student = {
+ name: "John",
+ email: "john@gmail.com",
+ phone: "518-444-5555",
+ address: {
+   number: 33,
+   street: "Christopher St",
+   city: "New York",
+   state: "NY",
+   zip: "12345"
+  }
+};
+
+
+/* 
+
+Exercise 03: Add to the student object and attribute named "classes" with the following list of entries:
+
+  math
+  history
+  design
+  accounting
+
+Then write the console.log lines to print out all its attributes.
+
+*/
